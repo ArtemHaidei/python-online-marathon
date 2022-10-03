347fa04a1d4ab75608b8b5f2d40c4d3966f6c3dd
Artem <haideiartem@gmail.com>
2022-10-04 01:16:17 +0300

    Pass quize 1-3 for sprint 02

---
+++ sprint_02/00_all_fuctions_sprint_02.py
@@ -0,0 +1,27 @@
+#test 1
+# def double_string(data):
+# 	counter = []
+
+# 	for x in range(len(data)):
+# 		chek_list = []
+
+# 		for i in range(len(data)):
+# 			item = data[x] + data[i]
+
+# 			if item in data and item not in chek_list:
+# 				chek_list.append(item)
+# 		counter += chek_list
+
+# 	return len(counter)
+
+
+#test 2
+# def morse_number(str_num):
+# 	result = []
+# 	for x in str_num:
+# 		if int(x) <= 5:
+# 			result.append('-----'.replace('-', '.', int(x)))
+# 		else:
+# 			result.append('-----'.replace('-', '.',10 - int(x))[::-1])
+
+# 	return ' '.join(result)

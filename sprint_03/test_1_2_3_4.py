77ad466097f7c4af36c0eac2fc6a28f0ea8645f3
Artem <haideiartem@gmail.com>
2022-10-07 01:11:56 +0300

    finish sprint_03

--- sprint_03/test_1_2_3_4.py
+++ sprint_03/test_1_2_3_4.py
@@ -1,71 +1,105 @@
 #test 1
 # def outer(name):
 #     def inner():
-#         print(f'Hello, {name}')
+#         print(f'Hello, {name}!')
     
 #     return inner
 
 
-# outer("Tom")()
-
-
-
 #test 2
 #def create(string1): return lambda string2: string1 == string2
- 
+
 
 #test 3
-
-# import string
-
-
-# letters_and_digits = string.ascii_lowercase + string.digits + '@._'
-
-
-# def create_account(user_name, password, secret_words):
-#     def chek():
-#         flag = False
-
-#         if not set(password) <= set(letters_and_digits) or not any([x.islower() for x in password]) or not len(password) >= 6 \
-#                 or not any([[x.isupper() for x in password]]) or not any([x in string.digits for x in password]):
-#                 flag = Ture
-
-
-
-        
-
+# from re import search
+#
+#
+# def create_account(user_name: str, password: str, secret_words: list):
+#     if len([x for x in [r'[a-z]', r'[A-Z]', r'[0-9]', r'[^a-zA-Z0-9]'] if search(x, password)]) != 4 \
+#             or len(password) < 6:
+#         raise ValueError
+#
+#     def check(check_pasword, check_words):
+#         if check_pasword != password:
+#             return False
+#
+#         if len(check_words) != len(secret_words):
+#             return False
+#
+#         [check_words.remove(x) for x in secret_words if x in check_words]
+#         if len(check_words) > 1:
+#             return False
+#
+#         return True
 #     return check
-
-
-
-# tom = create_account("Tom", "Qwerty1_", ["1", "word"])  
-# check1 = tom("Qwerty1_",  ["1", "word"]) 
-# check2 = tom("Qwerty1_",  ["word"]) 
-# check3 = tom("Qwerty1_",  ["word", "2"]) 
-# check4 = tom("Qwerty1!",  ["word", "12"]) 
-
+#
+#
+# tom = create_account("Tom", "Qwerty1_", ["1", "word"])
+# check1 = tom("Qwerty1_", ["1", "word"])
+# check2 = tom("Qwerty1_", ["word"])
+# check3 = tom("Qwerty1_", ["word", "2"])
+# check4 = tom("Qwerty1!", ["word", "12"])
 
 
 #test 4
-def divisor(n):
-    def inner():
-        lst = [x for x in range(1, n + 1) if n % x == 0]
+# def divisor(n):
+#     return iter([x for x in range(1, n + 1) if n % x == 0] + [None, None])
+#
+# three = divisor(3)
+# print(next(three))
+# print(next(three))
+# print(next(three))
 
 
+#test 5
+# def logger(func):
+#     def wrapper(*args, **kwargs):
+#         lst = [str(x) for x in args]
+#         if kwargs: lst += [str(x) for x in kwargs.values()]
+#
+#         if func(*args, **kwargs) is None:
+#             print(f'Executing of function {func.__name__} with arguments {", ".join(lst)}...')
+#             return
+#         print(f'Executing of function {func.__name__} with arguments {", ".join(lst)}...')
+#         return func(*args, **kwargs)
+#     return wrapper
+#
+#
+# @logger
+# def concat(*args, **kwargs):
+#     lst = [str(x) for x in args]
+#     if kwargs: lst += [str(x) for x in kwargs.values()]
+#     return ''.join(lst)
+#
+#
+# @logger
+# def sum(a, b):
+#     return a + b
+#
+#
+# @logger
+# def print_arg(arg):
+#     print(arg)
+#
+#
+# print(concat(1))
+# print(concat('first string', second=2, third='second string'))
+# print(concat('first string', {'first kwarg' :0, 'second kwarg': 'second kwarg'}))
+# print(sum(2, 3))
+# print_arg(2)
 
-three = divisor(3)
-print(next(three))
-print(next(three))
-print(next(three))
 
-
-
-
-
-
-
-
-
-
-
-
+# test 6
+# from random import shuffle
+#
+#
+# def randomWord(lst):
+#     if len(lst) == 0:
+#         lst += [None]
+#
+#     shuffle(lst)
+#     for x in lst:
+#         yield x
+#
+#     for x in randomWord(lst):
+#         yield x
